2ec729123b5de285049ec8bfad8dc43a3f1c146a
baoyujin <bjbaoyujin@corp.netease.com>
2017-12-19 13:56:03 +0800

    创建项目

---
+++ YJCardImageSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJCardImageSwitch
+//
+//  Created by 包宇津 on 2017/12/19.
+//  Copyright © 2017年 baoyujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
